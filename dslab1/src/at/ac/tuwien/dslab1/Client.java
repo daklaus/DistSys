611e6e4403eee90367b56d67045b540da08f159e
Klaus Krapfenbauer <klaus.krapfenbauer@gmx.com>
2012-10-08 23:55:03 +0200

    Initialized project

---
+++ dslab1/src/at/ac/tuwien/dslab1/Client.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package at.ac.tuwien.dslab1;
+
+/**
+ * @author klaus
+ *
+ */
+public class Client {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
